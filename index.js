16532a05b8c9cae7cf976b713fb7e67e00fed3ae
Estro <hugo.dalger@epitech.eu>
2018-09-20 16:23:43 +0200

    Fixing

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ const express = require('express');
 const app = express();
 const puppeteer = require('puppeteer');
 
+app.use(express.static('public'));
+
 app.get('/', async (request, response) => {
-  try {
-      app.use(express.static('public'));
-      response.send(`<body bgcolor="#F0FFFF"><div style="text-align: center;vertical-align: middle;"><img class="logo" src="/images/hello.gif" alt="Star" height="360" width="600"></div><br style="line-height: 10"><div style="text-align: center;vertical-align: middle;"><button onclick="window.location.href='/hello'"><b>Get some quotes</b></button></div></img></body>`);
-  } catch(e) {
-    console.log(e)
-  }
+    try {
+        response.send(`<body bgcolor="#F0FFFF"><div style="text-align: center;vertical-align: middle;"><img class="logo" src="/images/hello.gif" alt="Star" height="360" width="600"></div><br style="line-height: 10"><div style="text-align: center;vertical-align: middle;"><button onclick="window.location.href='/hello'"><b>Get some quotes</b></button></div></img></body>`);
+    } catch(e) {
+        console.log(e)
+    }
 });
 
 let getQuote = async () => {
@@ -26,14 +27,13 @@ let getQuote = async () => {
 };
 
 app.get('/hello', async (request, response) => {
-  try {
-    app.use(express.static('public'));
-    let quote = await getQuote();
-      response.send(`<body background = "/images/black.png"><div style="text-align: center;vertical-align: middle;"><img class="logo" src="/images/heading.png" alt="Star" height="360" width="600"></div><div style="text-align: center;vertical-align: middle;line-height: 90px; color: white; font-size: large;>` + quote +
-          `<br><br><button onClick=location=URL><b>refresh</b></button></div></img></body>`);
-  } catch (e) {
-      console.log(e);
-  }
+    try {
+        let quote = await getQuote();
+        response.send(`<body background = "/images/black.png"><div style="text-align: center;vertical-align: middle;"><img class="logo" src="/images/heading.png" alt="Star" height="360" width="600"></div><div style="text-align: center;vertical-align: middle;line-height: 90px; color: white; font-size: large;>` + quote +
+            `<br><br><button onClick=location=URL><b>refresh</b></button></div></img></body>`);
+    } catch (e) {
+        console.log(e);
+    }
 });
 
 app.listen(3000, () => {
